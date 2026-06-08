bcfe31fa40a08b3716ed9a12a5be116f0526037c
kkatsuya <kkatsuya@C010423092.zipangu.cac.co.jp>
2026-06-08 16:09:46 +0900

    add:orderパッケージ追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderShowController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderShowController.java
@@ -1,12 +1,9 @@
 package jp.co.sss.shop.controller.client.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 class ClientOrderShowController {
-
-	@Autowired
 
 	public String showOrderList() {
 
