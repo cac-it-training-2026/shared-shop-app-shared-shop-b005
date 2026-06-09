3cdd67c51db26892bb5b646bda7014c4bbb4725f
ryomatsu <ryomatsu@C010423571.zipangu.cac.co.jp>
2026-06-09 16:11:54 +0900

    add:新規メソッド追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
@@ -2,6 +2,8 @@ package jp.co.sss.shop.controller.client.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -30,7 +32,7 @@ public class ClientUserRegistController {
 			//UserFormを新規作成
 			userform = new UserForm();
 			//権限をコピー
-			userform.setAuthority(((UserBean) session.getAttribute("user")).getAuthority());
+			//userform.setAuthority(((UserBean) session.getAttribute("user")).getAuthority());
 
 			//セッションに保存
 			session.setAttribute("userForm", userform);
@@ -55,6 +57,31 @@ public class ClientUserRegistController {
 			session.setAttribute("userForm", userform);
 		}
 		return "redirect:/client/user/regist/input";
+	}
+
+	@RequestMapping(path = "/client/user/regist/input", method = RequestMethod.GET)
+	public String ClientInput(Model model) {
+
+		//セッションに保存されているuserFormを取得
+		UserForm userform = (UserForm) session.getAttribute("userForm");
+		if (userform == null) {
+			// セッション情報がない場合、エラー
+			return "redirect:/syserror";
+		}
+
+		//前の画面で発生した入力エラー情報をセッションから取得する
+		BindingResult result = (BindingResult) session.getAttribute("result");
+		if (result != null) {
+			//セッションにエラー情報がある場合、エラー情報をスコープに設定
+			model.addAttribute("org.springframework.validation.BindingResult.userForm", result);
+			//セッションのエラー情報を削除
+			session.removeAttribute("result");
+		}
+
+		//入力情報をスコープに保存
+		model.addAttribute("userForm", userform);
+
+		return "client/user/regist_input";
 	}
 
 }
