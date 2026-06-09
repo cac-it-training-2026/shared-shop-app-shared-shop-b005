e5ec8e1aa44753836b948fd94cd9d29fbcf041cb
inumaruh <inumaruh@C010422856.zipangu.cac.co.jp>
2026-06-09 16:07:41 +0900

    Update:ClientUserUpdateControllerメソッド追記

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
@@ -18,7 +18,7 @@ import jp.co.sss.shop.repository.UserRepository;
 import jp.co.sss.shop.util.Constant;
 
 @Controller
-class ClientUserUpdateController {
+public class ClientUserUpdateController {
 
 	/**
 	 * 会員情報リポジトリ
@@ -37,7 +37,7 @@ class ClientUserUpdateController {
 	 * @param id
 	 * @return
 	 */
-	@RequestMapping(path = "/client/user/update/input/{id}", method = RequestMethod.POST)
+	@RequestMapping(path = "/client/user/update/input", method = RequestMethod.POST)
 	public String updateInputInit(@PathVariable Integer id) {
 
 		// セッションスコープより情報を取り出す
@@ -54,7 +54,7 @@ class ClientUserUpdateController {
 			// 情報をコピー
 			BeanUtils.copyProperties(user, userForm);
 			// 変更入力フォームをセッションに保持
-			session.setAttribute("clientUserForm", userForm);
+			session.setAttribute("userForm", userForm);
 
 		}
 		return "redirect:/client/user/update/input";
@@ -69,12 +69,12 @@ class ClientUserUpdateController {
 	public String updateInput(Model model) {
 
 		//セッションから入力フォーム取得
-		UserForm clientUserForm = (UserForm) session.getAttribute("clientUserForm");
-		if (clientUserForm == null) {
+		UserForm userForm = (UserForm) session.getAttribute("userForm");
+		if (userForm == null) {
 
 			return "redirect:/syserror";
 		}
-		model.addAttribute("clientUserForm", clientUserForm);
+		model.addAttribute("userForm", userForm);
 
 		BindingResult result = (BindingResult) session.getAttribute("result");
 		if (result != null) {
@@ -88,29 +88,62 @@ class ClientUserUpdateController {
 
 	/**
 	 * 変更確認処理
+	 * 
 	 * @param form
 	 * @param result
 	 * @return
 	 */
 	@RequestMapping(path = "/client/user/update/check", method = RequestMethod.POST)
 	public String updateInputCheck(@Valid @ModelAttribute UserForm form, BindingResult result) {
+
+		UserForm lastUserForm = (UserForm) session.getAttribute("userForm");
+		if (lastUserForm == null) {
+			return "redirect:/syserror";
+		}
+		// 入力フォーム情報をセッションに保持
+		session.setAttribute("userForm", form);
+
+		// 入力値にエラーがあった場合、入力画面に戻る
+		if (result.hasErrors()) {
+
+			session.setAttribute("result", result);
+
+			//変更入力画面　表示処理
+			return "redirect:/client/user/update/input";
+		}
 		return "redirect:/client/user/update/check";
 	}
 
 	/**
+	 * 確認画面表示
+	 * 
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping(path = "/client/user/update/check", method = RequestMethod.GET)
 	public String updateCheck(Model model) {
+		// セッションから入力フォーム情報取得
+		UserForm userForm = (UserForm) session.getAttribute("userForm");
+		if (userForm == null) {
+			// セッション情報がない場合、エラー
+			return "redirect:/syserror";
+		}
+		model.addAttribute("userForm", userForm);
+
 		return "client/user/update_check";
 	}
 
+	/**
+	 * @return
+	 */
 	@RequestMapping(path = "/client/user/update/complete", method = RequestMethod.POST)
 	public String updateComplete() {
 		return "redirect:/client/user/update/complete";
 	}
 
+	/**
+	 * @return
+	 */
 	@RequestMapping(path = "/client/user/update/complete", method = RequestMethod.GET)
 	public String updateCompleteFinish() {
 		return "client/user/update_complete";
