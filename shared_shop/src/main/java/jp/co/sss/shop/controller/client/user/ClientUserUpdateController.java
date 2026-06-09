2ed16196121003834911dad7b0030bca3230b200
inumaruh <inumaruh@C010422856.zipangu.cac.co.jp>
2026-06-09 10:50:45 +0900

    update:ClientUserUpdateControllerメソッド更新

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
@@ -23,30 +23,49 @@ class ClientUserUpdateController {
 	@Autowired
 	HttpSession session;
 
+	/**
+	 * @param id
+	 * @return
+	 */
 	@RequestMapping(path = "/client/user/update/input/{id}", method = RequestMethod.POST)
 	public String updateInputInit(@PathVariable Integer id) {
 		return "redirect:/client/user/update/input";
 	}
 
+	/**
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(path = "client/user/update/input", method = RequestMethod.GET)
 	public String updateInput(Model model) {
 		return "client/user/update_input";
 	}
 
+	/**
+	 * @param form
+	 * @param result
+	 * @return
+	 */
 	@RequestMapping(path = "/client/user/update/check", method = RequestMethod.POST)
 	public String updateInputCheck(@Valid @ModelAttribute UserForm form, BindingResult result) {
 		return "redirect:/client/user/update/check";
 	}
 
-	@RequestMapping(path = "/client/user/update/check", method = RequestMethod.POST)
+	/**
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(path = "/client/user/update/check", method = RequestMethod.GET)
 	public String updateCheck(Model model) {
 		return "client/user/update_check";
 	}
 
+	@RequestMapping(path = "/client/user/update/complete", method = RequestMethod.POST)
 	public String updateComplete() {
 		return "redirect:/client/user/update/complete";
 	}
 
+	@RequestMapping(path = "/client/user/update/complete", method = RequestMethod.GET)
 	public String updateCompleteFinish() {
 		return "client/user/update_complete";
 	}
